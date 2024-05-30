263e0d2408eea23a1162fed11ab19ce78b62cfec
Егор <167519650+Rejis3@users.noreply.github.com>
2024-05-30 10:14:06 +0300

    task_exem.py

--- task_exem.py
+++ task_exem.py
@@ -55,3 +55,21 @@ for i in range(1, 11):
 print(k)
 print(l)
 print(f'Колличество отрицательных элементов массива l = {len([value for value in l if value < 0])}')
+
+k, l = list(range(1, 11)), list(range(1, 11))
+
+for i in range(1, 11):
+    k[i - 1] = 10 - i
+for i in range(1, 11):
+    l[i - 1] = k[5] - k[i - 1]
+print(k)
+print(l)
+count_k = 0
+for value in k:
+    if value < 0
+        count_k += 1
+count_l - 0
+for value in l:
+    if value < 0:
+        count_l += 1
+print(f'Количество отрицательных элементов = {count_k + count_l}')
